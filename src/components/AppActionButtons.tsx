099f07d2bc7a5850ab5d8075e3198aad42771b69
liljdot <jehuoki@gmail.com>
2025-03-24 23:34:10 +0100

    Refactor DownloadAppSection and AppActionButtons to fit mobile and tablet screens

--- src/components/AppActionButtons.tsx
+++ src/components/AppActionButtons.tsx
@@ -7,24 +7,24 @@ interface Props {
 
 export const PlayStoreButton: React.FC<Props> = ({to}) => {
 
-    return <Link to={to} className="btn btn-secondary w-50 flex flex-row justify-between text-neutral rounded-full py-6.5 transition-all ease-in-out duration-300 hover:scale-105">
-        <FaGooglePlay className="size-7"/>
+    return <Link to={to} className="btn btn-secondary w-45 md:w-50 flex flex-row justify-between text-neutral rounded-full py-6.5 transition-all ease-in-out duration-300 hover:scale-105">
+        <FaGooglePlay className="size-6 md:size-7"/>
         
         <div className="flex flex-col font-normal text-left">
             <p className="text-xs">GET IT ON</p>
-            <p className="text-2xl">Google Play</p>
+            <p className="text-xl md:text-2xl">Google Play</p>
         </div>
     </Link>
 }
 
 export const AppStoreButton: React.FC<Props> = ({to}) => {
 
-    return <Link to={to} className="btn btn-secondary w-50 flex flex-row justify-between text-neutral rounded-full py-6.5 transition-all ease-in-out duration-300 hover:scale-105">
-        <FaApple className="size-7.5"/>
+    return <Link to={to} className="btn btn-secondary w-45 md:w-50 flex flex-row justify-between text-neutral rounded-full py-6.5 transition-all ease-in-out duration-300 hover:scale-105">
+        <FaApple className="size-6.5 md:size-7.5"/>
         
         <div className="flex flex-col font-normal text-left">
             <p className="text-xs">DOWNLOAD ON THE</p>
-            <p className="text-2xl">App Store</p>
+            <p className="text-xl md:text-2xl">App Store</p>
         </div>
     </Link>
 }
--- src/pages/Home/DownloadAppSection.tsx
+++ src/pages/Home/DownloadAppSection.tsx
@@ -13,14 +13,14 @@ const DownloadAppSection: React.FC = () => {
 
     return (
         <>
-            <section className="relative bg-warning flex flex-row justify-around pt-17 px-17">
+            <section className="relative bg-warning flex flex-col md:flex-row justify-around pb-30 lg:pb-10 xl:pb-0 pt-16 md:pt-17 px-5 md:px-17 rounded-xl">
                 <img src={theme == "dark" ? atomImageDark : atomImage} alt="atom" className="absolute top-0 right-0"/>
                 <img src={theme == "dark" ? atomImage2Dark : atomImage2} alt="atom" className="absolute bottom-0 left-0"/>
 
                 <div className="flex flex-col gap-7 text-secondary max-w-202">
-                    <h2 className="text-5xl font-extrabold">Run Your Business from Your Pocket: Anytime, Anywhere.</h2>
+                    <h2 className="text-2xl/10 md:text-3xl lg:text-5xl font-extrabold">Run Your Business From Your Pocket: Anytime, Anywhere.</h2>
 
-                    <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-4 text-sm sm:[1rem]">
                         <div className="flex flex-row gap-3 items-center">
                             <IoStar className="text-[gold] size-5"/>
 
@@ -40,14 +40,14 @@ const DownloadAppSection: React.FC = () => {
                         </div>
                     </div>
 
-                    <div className="relative flex flex-row gap-6 z-1">
+                    <div className="relative flex flex-row justify-around sm:justify-start gap-3 sm:gap-6 z-1">
                         <AppStoreButton to=""/>
 
                         <PlayStoreButton to=""/>
                     </div>
                 </div>
 
-                <figure>
+                <figure className="mt-14 md:mt-0">
                     <img src={theme == "dark" ? phoneImage : phoneImageLight} />
                 </figure>
             </section>
