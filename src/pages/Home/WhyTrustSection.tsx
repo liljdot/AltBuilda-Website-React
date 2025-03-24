51bf8bd34c3b768f82498a8de6b84465a28d41ed
liljdot <jehuoki@gmail.com>
2025-03-24 20:22:45 +0100

    Refactor WhyTrustSection to fit mobile and tablet displays

--- src/pages/Home/WhyTrustSection.tsx
+++ src/pages/Home/WhyTrustSection.tsx
@@ -2,9 +2,9 @@ const Achievment: React.FC<{ value: string, achievment: string }> = ({value, ach
 
     return (
         <div className="card bg-transparent">
-            <div className="card-body text-center flex flex-col gap-5">
-                <h2 className="text-7xl font-bold">{value}+</h2>
-                <p className="text-lg">{achievment}</p>
+            <div className="card-body text-center flex flex-col items-center gap-3 md:gap-5">
+                <h2 className="text-4xl md:text-7xl font-bold">{value}+</h2>
+                <p className="md:text-lg">{achievment}</p>
             </div>
         </div>
     )
@@ -14,10 +14,10 @@ const WhyTrustSection: React.FC = () => {
 
     return (
         <>
-            <section className="w-full flex flex-col justify-center relative bg-accent py-25 z-0 before:absolute before:h-[66%] before:bg-neutral before:w-[140%] before:-translate-y-[110%] before:rounded-b-[50%] before:self-center before:z-1 after:absolute after:h-[66%] after:bg-neutral after:w-[140%] after:translate-y-[110%] after:rounded-t-[50%] after:self-center after:z-1">
-                <h3 className="text-5xl text-center font-bold px-75">Why Small Medium Enterprises Trust Altbuilda.</h3>
+            <section className="w-full flex flex-col justify-center relative bg-accent py-22.5 sm:py-25 z-0 before:absolute before:h-[20%] sm:before:h-[66%] before:bg-neutral before:w-[140%] before:-translate-y-[270%] sm:before:-translate-y-[110%] before:rounded-b-[50%] before:self-center before:z-1 after:absolute after:h-[20%] sm:after:h-[66%] after:bg-neutral after:w-[140%] after:translate-y-[270%] sm:after:translate-y-[110%] after:rounded-t-[50%] after:self-center after:z-1">
+                <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-5xl text-center font-bold px-6 lg:px-50 xl:px-75">Why Small Medium Enterprises Trust Altbuilda.</h3>
 
-                <div className="grid grid-cols-3 px-37.5 mt-23 gap-y-39">
+                <div className="grid grid-cols-2 lg:grid-cols-3 px-5.5 lg:px-37.5 mt-12 lg:mt-23 gap-y-15 lg:gap-y-39">
                     <Achievment value="10,000" achievment="Businesses Served"/>
                     <Achievment value="50" achievment="SME Industries Supported"/>
                     <Achievment value="$100,000" achievment="In Total Transactions Processed"/>
