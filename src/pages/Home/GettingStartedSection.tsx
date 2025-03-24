7c46f2afe2b34a185effe81d3c22a6c7be30300d
liljdot <jehuoki@gmail.com>
2025-03-24 21:46:44 +0100

    Refactor GettingStartedSection to fit mobile and tablet screens

--- src/pages/Home/GettingStartedSection.tsx
+++ src/pages/Home/GettingStartedSection.tsx
@@ -5,16 +5,16 @@ const GettingStartedSection: React.FC = () => {
 
     return (
         <>
-            <div className="flex flex-col items-center py-12.5">
-                <div className="flex flex-col items-center gap-6 text-secondary text-center">
-                    <h1 className="text-5xl font-bold">Getting Started is Easy!</h1>
+            <div className="flex flex-col items-center py-12 md:py-12.5">
+                <div className="flex flex-col items-center gap-6 text-secondary text-center px-6.5 md:px-0">
+                    <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-bold">Getting Started is Easy!</h1>
 
-                    <p className="text-lg">Sign up, and start managing your business effortlessly with Altbuilda’s powerful tools.</p>
+                    <p className="md:text-lg">Sign up, and start managing your business effortlessly with Altbuilda’s powerful tools.</p>
 
                     <img src={signVector} alt="Sign" width={172}/>
                 </div>
 
-                <div className="mt-17">
+                <div className="sm:mt-17">
                     <StepsImage />
                 </div>
             </div>
