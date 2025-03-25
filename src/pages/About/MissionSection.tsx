5f6984804c2b3a63c2ad760b6964d8ae495324fb
liljdot <jehuoki@gmail.com>
2025-03-25 11:17:43 +0100

    Add Mission Section to about Page

--- src/pages/About/MissionSection.tsx
+++ src/pages/About/MissionSection.tsx
@@ -1,8 +1,34 @@
+import missionImage from "../../assets/mission-image.png"
+import missionImageMobile from "../../assets/mission-image-mobile.png"
+import ActionButton from "../../components/ActionButton";
+
 const MissionSection: React.FC = () => {
 
     return (
         <>
-        
+            <section className="w-full bg-neutral py-26">
+                <div className="text-center">
+                    <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-medium">Our Mission</h3>
+                </div>
+
+                {/* hidden on mobile */}
+                <figure className="hidden sm:block">
+                    <img src={missionImage} alt="AltBuilda Image" />
+                </figure>
+
+                {/* for mobile  */}
+                <figure className="sm:hidden mt-5">
+                    <img src={missionImageMobile} alt="AltBuilda Image" />
+                </figure>
+
+                <p className="text-center text-secondary text-xl/10 px-39 mt-19">
+                    Altbuida is designed to empower Small and Medium Enterprises (SMEs) like yours to thrive and scale. We provide the essential tools, financial solutions, and networking opportunities you need to manage your business effectively and achieve sustainable growth. Whether you're just starting or looking to expand, Altbuida is the right platform for success.
+                </p>
+
+                <div className="flex flex-row justify-center my-14">
+                    <ActionButton to="/services" filled>Learn More</ActionButton>
+                </div>
+            </section>
         </>
     )
 }
