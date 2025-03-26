f10531b3fcc130bebdd912865e8267515caf746e
liljdot <jehuoki@gmail.com>
2025-03-26 01:00:27 +0100

    Refactor OfferSection component for improved layout and responsiveness

--- src/pages/About/OfferSection.tsx
+++ src/pages/About/OfferSection.tsx
@@ -18,16 +18,16 @@ const OfferCard: React.FC<OfferCardProps> = ({ Icon, title, children }) => {
         <>
             <div className="flex flex-row gap-8">
                 <div className="flex">
-                    <div className="bg-primary p-3 rounded-lg size-fit">
+                    <div className="bg-primary p-3 rounded-xl size-12 lg:size-12">
                         <Icon size={6} />
                     </div>
                 </div>
 
-                <div className="card text-primary-content w-104">
+                <div className="card text-primary-content lg:w-104 xl:w-full">
                     <div className="card-body p-0 text-secondary gap-3 font-medium">
-                        <h2 className="card-title text-2xl">{title}</h2>
+                        <h2 className="card-title md:text-2xl">{title}</h2>
 
-                        <p>{children}</p>
+                        <p className="text-sm md:text-[1rem]">{children}</p>
                     </div>
                 </div>
             </div>
@@ -40,8 +40,8 @@ const OfferSection: React.FC = () => {
 
     return (
         <>
-            <section className="w-full bg-neutral border-9 border-dashed border-secondary border-b-0 p-20">
-                <div className="flex flex-col gap-6 text-center text-secondary items-center">
+            <section className="w-full bg-neutral border-3 md:border-9 border-dashed border-secondary border-b-0 px-8 py-17 md:px-20 md:py-20">
+                <div className="flex flex-col gap-3 md:gap-6 text-center text-secondary items-center">
                     <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-bold">What We Offer</h3>
 
                     <p className="md:text-lg">Your All-in-One Business Solution</p>
@@ -49,7 +49,7 @@ const OfferSection: React.FC = () => {
                     <img src={signVector} alt="Sign" className="w-43" />
                 </div>
 
-                <div className="flex flex-row justify-between mt-20">
+                <div className="flex flex-col items-center gap-10 lg:flex-row justify-between mt-12 md:mt-20">
                     <div className="flex flex-col gap-8">
                         <OfferCard Icon={BusinessMangementIcon} title="Business Management Tools">
                             Streamline your daily operations with our suite of tools. From automated invoicing and tax compliance support to financial tracking, gain real-time insights and make data-driven decisions.
@@ -64,12 +64,12 @@ const OfferSection: React.FC = () => {
                         </OfferCard>
                     </div>
 
-                    <figure className="w-165">
+                    <figure className="w-full md:w-165 xl:w-full">
                         <img src={theme == "dark" ? offeringsImageDark : offeringsImage} alt="What we offer" />
                     </figure>
                 </div>
 
-                <div className="flex flex-row justify-center my-29">
+                <div className="flex flex-row justify-center my-12.5 md:my-29">
                     <ActionButton to="" filled shrink>Explore our Features</ActionButton>
                 </div>
             </section>
