7ef201e5b192c126bd159a694dc25044913f1bc7
liljdot <jehuoki@gmail.com>
2025-03-26 23:28:55 +0100

    Refactor FAQBanner layout and typography mobile and tablet screens

--- src/pages/FAQ/FAQBanner.tsx
+++ src/pages/FAQ/FAQBanner.tsx
@@ -2,15 +2,15 @@ const FAQBanner: React.FC = () => {
 
     return (
         <>
-            <section className="w-full bg-base-100 flex flex-row justify-between px-20 py-24">
-                <div className="flex flex-col gap-3 px-8">
-                    <p className="text-primary font-semibold">FIND QUICK ANSWERS TO COMMON QUESTIONS</p>
+            <section className="w-full bg-base-100 flex flex-col lg:flex-row gap-4 md:gap-0 md:justify-between px-4 lg:px-20 py-16 lg:py-24">
+                <div className="flex flex-col gap-3  sm:px-8">
+                    <p className="text-primary text-sm sm:text-[1rem] font-semibold">FIND QUICK ANSWERS TO COMMON QUESTIONS</p>
 
-                    <h3 className="text-5xl text-secondary font-bold">FAQ</h3>
+                    <h3 className="text-3xl md:text-5xl text-secondary font-medium md:font-bold">FAQ</h3>
                 </div>
 
-                <div className="flex flex-col justify-end px-8">
-                    <p className="text-xl text-secondary">Have questions? We’re here to help.</p>
+                <div className="flex flex-col md:justify-end sm:px-8">
+                    <p className="md:text-xl text-secondary">Have questions? We’re here to help.</p>
                 </div>
             </section>
         </>
