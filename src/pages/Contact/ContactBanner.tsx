948f9970bc52431f0eeea1fb99dd256987d506af
liljdot <jehuoki@gmail.com>
2025-03-27 13:17:57 +0100

    Refactor ContactBanner component to fit mobile and tablet screens

--- src/pages/Contact/ContactBanner.tsx
+++ src/pages/Contact/ContactBanner.tsx
@@ -6,27 +6,27 @@ const ContactBanner: React.FC = () => {
 
     return (
         <>
-            <section className="w-full bg-cover bg-no-repeat flex flex-row justify-between px-14 py-9" style={{ backgroundImage: `url(${bannerGifBG})` }}>
-                <div className="flex flex-col gap-3">
-                    <p className="text-primary">Get Started</p>
+            <section className="w-full bg-cover bg-no-repeat flex flex-col md:flex-row gap-9 md:gap-0 md:justify-between md:px-14 pt-12 md:pt-9 pb-3.5 md:pb-9" style={{ backgroundImage: `url(${bannerGifBG})` }}>
+                <div className="flex flex-col gap-3 text-center md:text-left">
+                    <p className="text-primary text-sm md:text-[1rem] font-semibold ">Get Started</p>
 
-                    <h3 className="text-5xl/15 text-secondary font-bold">
+                    <h3 className=" text-2xl md:text-5xl/15 text-white font-semibold md:font-bold">
                         Get in touch with us.
                         <br />
                         We're here to assist you.
                     </h3>
                 </div>
 
-                <div className="flex flex-col gap-6 self-center py-9">
-                    <Link to={""} className="size-12.5 border border-secondary rounded-full flex justify-center items-center">
+                <div className="flex flex-row md:flex-col gap-6 self-center md:py-9">
+                    <Link to={""} className="size-12.5 text-white border border-white rounded-full flex justify-center items-center">
                         <FaFacebookF className="size-3.5" />
                     </Link>
 
-                    <Link to={""} className="size-12.5 border border-secondary rounded-full flex justify-center items-center">
+                    <Link to={""} className="size-12.5 text-white border border-white rounded-full flex justify-center items-center">
                         <FaInstagram className="size-3.5" />
                     </Link>
 
-                    <Link to={""} className="size-12.5 border border-secondary rounded-full flex justify-center items-center">
+                    <Link to={""} className="size-12.5 text-white border border-white rounded-full flex justify-center items-center">
                         <FaTwitter className="size-3.5" />
                     </Link>
                 </div>
