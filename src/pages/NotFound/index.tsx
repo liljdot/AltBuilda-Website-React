15338e0d90b0482f9135e8738782bb0a932fd633
liljdot <jehuoki@gmail.com>
2025-03-27 19:09:39 +0100

    Refactor NotFound page layout and styles to fit mobile and tablet

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -3,26 +3,26 @@ import image404Dark from "../../assets/404-image-dark.png"
 import useThemeContext from "../../hooks/useThemeContext"
 
 const NotFound: React.FC = () => {
-    const {theme} = useThemeContext()
+    const { theme } = useThemeContext()
 
     return (
         <>
             <main className="p-0 bg-neutral w-full">
                 <div className="max-w-450 mx-auto">
-                    <section className="w-full bg-base-100 flex flex-row px-20 py-40">
-                        <div className="flex flex-col w-[50%] justify-center px-8">
-                            <h6 className="text-primary text-left font-medium">404 error</h6>
+                    <section className="w-full bg-base-100 flex flex-col-reverse gap-8 lg:gap-0 lg:flex-row px-4 sm:px-8 lg:px-20 py-16 sm:py-24 lg:py-40">
+                        <div className="flex flex-col gap-3 lg:gap-0 lg:w-[50%] justify-center md:px-8">
+                            <h6 className="text-primary xl:text-xl font-semibold md:font-medium">404 error</h6>
 
-                            <h1 className="text-secondary text-6xl/20 font-bold">Oops! We Couldn't Find That Page.</h1>
+                            <h1 className="text-secondary text-4xl xl:text-6xl/20 font-medium md:font-bold">Oops! We Couldn't Find That Page.</h1>
 
-                            <p className="text-secondary text-xl/10">
+                            <p className="text-secondary text-lg xl:text-xl/10">
                                 It seems like the page you were looking for is missing or may
                                 have been moved. But don't worry, we're here to help!
                             </p>
                         </div>
 
-                        <figure className="flex flex-col w-[50%] justify-center px-8">
-                            <img src={theme == "dark" ? image404Dark : image404} alt="404" />
+                        <figure className="flex flex-col lg:w-[50%] justify-center md:px-8">
+                            <img src={theme == "dark" ? image404Dark : image404} alt="404" className="w-[80%] lg:w-full"/>
                         </figure>
                     </section>
                 </div>
