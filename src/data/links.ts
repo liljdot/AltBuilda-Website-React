252c9082c40d3c6aca188e286e583106ee85dbe9
liljdot <jehuoki@gmail.com>
2025-07-09 18:39:48 +0100

    feat(links): update app login and register page URLs

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,2 +1,2 @@
-export const appLoginPage = ""
-export const appRegisterPage = ""
+export const appLoginPage = "https://alt-builda-frontend-user.vercel.app/login"
+export const appRegisterPage = "https://alt-builda-frontend-user.vercel.app/register"
