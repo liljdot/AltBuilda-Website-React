2069c778064720c22f13720b5297fab1fc24c83c
liljdot <jehuoki@gmail.com>
2025-10-02 15:41:12 +0100

    setup query client

--- src/App.tsx
+++ src/App.tsx
@@ -13,28 +13,32 @@ import BlogPostPage from "./pages/BlogPostPage"
 import { Toaster } from "react-hot-toast"
 import PrivacyPolicy from "./pages/PrivacyPolicy"
 import TermsandConditions from "./pages/TermsandConditions"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 function App() {
   const { theme } = useThemeContext()
+  const queryClient = new QueryClient()
 
   return (
     <>
       <div className="bg-secondary w-screen items-center overflow-x-hidden" data-theme={theme}>
-        <Toaster />
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:id" element={<BlogPostPage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-and-conditions" element={<TermsandConditions />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
+        <QueryClientProvider client={queryClient}>
+          <Toaster />
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogPostPage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-and-conditions" element={<TermsandConditions />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </QueryClientProvider>
       </div>
     </>
   )
