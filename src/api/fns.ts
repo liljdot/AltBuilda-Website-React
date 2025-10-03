3984c0e70d8718802861a4b947dc1c0ab21b5bf5
liljdot <jehuoki@gmail.com>
2025-10-03 22:45:36 +0100

    create getprivacypolicy fn

--- src/api/fns.ts
+++ src/api/fns.ts
@@ -1,8 +1,27 @@
 import rejectJson from "./rejectJson"
 
+const baseURL = import.meta.env.VITE_API_BASE_URL
+
+export interface GetPrivacyPolicyResponse {
+    "success": boolean
+    "message": string
+    "remainingDraftsToGenerate": number
+    "statusCode": number
+    "result": {
+        "title": string
+        "introduction": string
+        "sections": {
+            "id": string
+            "title": string
+            "content": string
+            "order": number
+        }[]
+    }
+}
+
 const subscribeNewsletter = (email: string) => {
     return fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/api/v1/Admin/content-mgmt/blogs-newsletter/subscribe`,
+        `${baseURL}/api/v1/Admin/content-mgmt/blogs-newsletter/subscribe`,
         {
             credentials: "include",
             method: "POST",
@@ -14,6 +33,12 @@ const subscribeNewsletter = (email: string) => {
         .then(res => !res.ok ? rejectJson(res) : res.json())
 }
 
+const getPrivacyPolicy = () => {
+    return fetch(`${baseURL}/api/v1/Content/privacy-policy`)
+        .then(res => !res.ok ? rejectJson : res.json())
+}
+
 export {
-    subscribeNewsletter
+    subscribeNewsletter,
+    getPrivacyPolicy
 }
