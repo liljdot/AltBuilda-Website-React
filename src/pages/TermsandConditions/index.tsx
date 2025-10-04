ae318855865ad8df2889d04b283e8c817e3b9289
liljdot <jehuoki@gmail.com>
2025-10-04 12:38:14 +0100

    refactor privacy policy index page to query get terms endpoint for content and pass down result to content section

--- src/pages/TermsandConditions/index.tsx
+++ src/pages/TermsandConditions/index.tsx
@@ -1,16 +1,33 @@
+import { useQuery } from "@tanstack/react-query"
 import Banner from "../../components/Banner"
 import useDocumentTitle from "../../hooks/useDocumentTitle"
 import TermsContentSection from "./TermsContentSection"
+import { getTermsAndConditions } from "../../api/fns"
 
 const TermsandConditions: React.FC = () => {
     useDocumentTitle("T's & C's | AltBuilda")
+    const { data, isLoading } = useQuery({
+        queryFn: getTermsAndConditions,
+        queryKey: ["TermsAndConditions"]
+    })
 
     return (
         <>
             <main className="p-0 bg-neutral w-full">
                 <div className="max-w-450 mx-auto">
                     <Banner title="Terms and Conditions" page="CURRENT AS OF 27TH SEPTEMBER, 2025" />
-                    <TermsContentSection />
+                    {
+                        isLoading && (
+                            <div className="flex justify-center items-center min-h-50 md:min-h-100">
+                                <span className="loading loading-spinner loading-xl text-primary justify-self-center self-center"></span>
+                            </div>
+                        )
+                    }
+                    {
+                        data && <TermsContentSection
+                            document={data.result}
+                        />
+                    }
                 </div>
             </main>
         </>
