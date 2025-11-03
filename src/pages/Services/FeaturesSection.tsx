a613046e87bf4d7c5ca172714dac2946ec390a50
liljdot <jehuoki@gmail.com>
2025-11-03 20:37:22 +0100

    make newsletter feature responsive and extract into comp

--- src/pages/Services/FeaturesSection.tsx
+++ src/pages/Services/FeaturesSection.tsx
@@ -2,36 +2,38 @@ import featureImageRight from "../../assets/feature-image-right.png"
 import featureImageLeft from "../../assets/feature-image-left.png"
 
 import newsletterOnPhoneImage from "../../assets/newsletter-on-phone-image.png"
+import newsletterOnPhoneImageMobile from "../../assets/newsletter-on-phone-image-mobile.png"
+import newsletterOnPhoneImageMobileDark from "../../assets/newsletter-on-phone-image-mobile-dark.png"
 
 import chatIcon from "../../assets/chat-icon.png"
 import chatIconDark from "../../assets/chat-icon-dark.png"
 
-import altproIcon from "../../assets/altpro-icon.png"
-import altproIconDark from "../../assets/altpro-icon-dark.png"
+// import altproIcon from "../../assets/altpro-icon.png"
+// import altproIconDark from "../../assets/altpro-icon-dark.png"
 
-import invoiceIcon from "../../assets/invoice-icon.png"
-import invoiceIconDark from "../../assets/invoice-icon-dark.png"
+// import invoiceIcon from "../../assets/invoice-icon.png"
+// import invoiceIconDark from "../../assets/invoice-icon-dark.png"
 
-import speedIcon from "../../assets/speed-icon.png"
-import speedIconDark from "../../assets/speed-icon-dark.png"
+// import speedIcon from "../../assets/speed-icon.png"
+// import speedIconDark from "../../assets/speed-icon-dark.png"
 
-import noteIcon from "../../assets/note-icon.png"
-import noteIconDark from "../../assets/note-icon-dark.png"
+// import noteIcon from "../../assets/note-icon.png"
+// import noteIconDark from "../../assets/note-icon-dark.png"
 
-import bookIcon from "../../assets/book-icon.png"
-import bookIconDark from "../../assets/book-icon-dark.png"
+// import bookIcon from "../../assets/book-icon.png"
+// import bookIconDark from "../../assets/book-icon-dark.png"
 
-import peopleIcon from "../../assets/people-icon.png"
-import peopleIconDark from "../../assets/people-icon-dark.png"
+// import peopleIcon from "../../assets/people-icon.png"
+// import peopleIconDark from "../../assets/people-icon-dark.png"
 
-import soundIcon from "../../assets/sound-icon.png"
-import soundIconDark from "../../assets/sound-icon-dark.png"
+// import soundIcon from "../../assets/sound-icon.png"
+// import soundIconDark from "../../assets/sound-icon-dark.png"
 
-import chartIcon from "../../assets/chart-icon.png"
-import chartIconDark from "../../assets/chart-icon-dark.png"
+// import chartIcon from "../../assets/chart-icon.png"
+// import chartIconDark from "../../assets/chart-icon-dark.png"
 
-import buildingIcon from "../../assets/building-icon.png"
-import buildingIconDark from "../../assets/building-icon-dark.png"
+// import buildingIcon from "../../assets/building-icon.png"
+// import buildingIconDark from "../../assets/building-icon-dark.png"
 
 import iphoneImage from "../../assets/iphone-image.png"
 import iphoneImageDark from "../../assets/iphone-image-dark.png"
@@ -83,39 +85,13 @@ const FeatureCard: React.FC<FeatureCardProps> = ({ reverse, icon, title, content
 }
 
 const FeatureCardsSection: React.FC = () => {
-    const { theme } = useThemeContext()
 
     return (
         <>
-            <section className="bg-neutral flex flex-col items-center gap-12 md:gap-24 px-38 py-17 md:py-24">
-                <div className="card lg:card-side w-full xl:w-218">
-                    <div className="card-body w-1/2 p-0 justify-center">
-                        <div className="flex flex-col space-y-8">
-                            <figure className="size-12">
-                                <img src={theme == "dark" ? chatIconDark : chatIcon} />
-                            </figure>
+            <section className="bg-neutral flex flex-col items-center gap-12 md:gap-24 px-4 md:px-19 lg:px-38 py-17 md:py-24">
+                <NewsletterFeature />
 
-                            <h4 className="text-xl xl:text-3xl font-medium">Email Newsletter</h4>
-
-                            <p className="xl:text-xl">
-                                Personalized industry insights & updates
-                            </p>
-
-                            <Link to={"/blog#newsletter"} className="btn btn-primary text-secondary p-6.5 rounded-full min-w-55 md:min-w-fit max-w-67 hover:scale-105 transition-all ease-in-out duration-500">
-                                Subscribe Now
-                            </Link>
-                        </div>
-                    </div>
-                    <figure className="w-1/2">
-                        <img
-                            src={newsletterOnPhoneImage}
-                            alt="Newsletter"
-                        />
-                    </figure>
-
-                </div>
-
-                <FeatureCard
+                {/* <FeatureCard
                     title="Email Newsletter"
                     content="Personalized industry insights & updates"
                     icon={theme == "dark" ? chatIconDark : chatIcon}
@@ -179,9 +155,52 @@ const FeatureCardsSection: React.FC = () => {
                     title="Business Registration Support"
                     content="CAC guidance for new businesses"
                     icon={theme == "dark" ? buildingIconDark : buildingIcon}
-                />
+                /> */}
             </section>
         </>
+    )
+}
+
+const NewsletterFeature: React.FC = () => {
+    const { theme } = useThemeContext()
+
+    return (
+        <div className="card md:card-side lg:w-full xl:w-218 space-y-10">
+            <div className="card-body md:w-1/2 p-0 justify-center">
+                <div className="flex flex-col items-center md:items-start space-y-8">
+                    <figure className="size-12">
+                        <img src={theme == "dark" ? chatIconDark : chatIcon} />
+                    </figure>
+
+                    <h4 className="text-xl xl:text-3xl font-medium">Email Newsletter</h4>
+
+                    <p className="xl:text-xl">
+                        Personalized industry insights & updates
+                    </p>
+
+                    <Link to={"/blog#newsletter"} className="btn btn-primary text-secondary p-6.5 rounded-full min-w-55 md:min-w-fit w-67 hover:scale-105 transition-all ease-in-out duration-500">
+                        Subscribe Now
+                    </Link>
+                </div>
+            </div>
+
+            {/* for mobile and small tablets */}
+            <div className="md:hidden">
+                <img
+                    src={theme == "dark" ? newsletterOnPhoneImageMobileDark : newsletterOnPhoneImageMobile}
+                    alt="Newsletter"
+                />
+            </div>
+
+            {/* hidden on mobile and small tablets  */}
+            <div className="hidden md:flex w-1/2">
+                <img
+                    src={newsletterOnPhoneImage}
+                    alt="Newsletter"
+                />
+            </div>
+
+        </div>
     )
 }
 
